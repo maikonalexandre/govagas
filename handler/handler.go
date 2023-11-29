b6a8d6004291786fb66ccb240235468933dfde2c
Maikon Alexandre Dos Santos Sousa <maikon@pop-os.localdomain>
2023-11-29 15:27:32 -0300

    feat: separate handlers

--- handler/handler.go
+++ handler/handler.go
@@ -10,22 +10,9 @@ func HealthHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"message": "ok"})
 }
 
-func ShowOpeningHandler(ctx *gin.Context) {
 
-}
 
-func CreateOpeningHandler(ctx *gin.Context) {
 
-}
 
-func DeleteOpeningHandler(ctx *gin.Context) {
 
-}
 
-func UpdateOpeningHandler(ctx *gin.Context) {
-
-}
-
-func ListOpeningHandler(ctx *gin.Context) {
-
-}
